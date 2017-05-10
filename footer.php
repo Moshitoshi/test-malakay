1e553e12ad204f7e0f3b747dbf9412ae1daaba66
moisi guga <moisi.guga@gmail.com>
2017-05-10 08:30:31 +0200

    Aggiunto loden ipsum

--- footer.php
+++ footer.php
@@ -17,7 +17,9 @@
  		<div class="row">
  			<div class="col-md-12">
  				<footer class="site-footer" id="colophon">
-					<div class="col-md-8"></div>
+					<div class="col-md-8">
+					  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
 				<div class="site-info">
 					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'malakay' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'malakay' ), 'WordPress' ); ?></a>
 					<span class="sep"> | </span>
